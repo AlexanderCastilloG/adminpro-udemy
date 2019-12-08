15ab868ba2f656de4466e4fdd8cbb4a44c386085
Alexander <gcastilloalex@gmail.com>
2019-12-07 21:56:15 -0500

    Sección 13 terminada

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { UsuarioService } from '../services/usuario/usuario.service';
+import { Usuario } from '../models/usuario.model';
 
 // Llamar una función de un script desde afuera
 // Llamar cualquier scripts que se encuentre afuera de angular, con plugins, carruseles, etc
 declare function init_plugins();
+declare const gapi: any;
 
 @Component({
   selector: 'app-login',
@@ -12,14 +16,68 @@ declare function init_plugins();
 })
 export class LoginComponent implements OnInit {
 
-  constructor(public router: Router) { }
+  email: string;
+  recuerdame: boolean = false;
+
+  auth2: any;
+
+  constructor(public router: Router,
+              public _usuarioService: UsuarioService) { }
 
   ngOnInit() {
     init_plugins();
+    this.googleInit();
+
+    this.email = localStorage.getItem('email') || '';
+    if (this.email.length > 1) {
+      this.recuerdame = true;
+    }
   }
 
-  ingresar() {
-    this.router.navigate(['/dashboard']);
+  googleInit() {
+    gapi.load('auth2', () => {
+
+      this.auth2 = gapi.auth2.init({
+        client_id: '459421650483-1fv4248objac0ihobrdtcj3l80kpu8jr.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        scope: 'profile email'
+      });
+
+      this.attachSignin(document.getElementById('btnGoogle'));
+    });
+  }
+
+  attachSignin(element) {
+    this.auth2.attachClickHandler(element, {}, (googleUser) => {
+      // let profile = googleUser.getBasicProfile();
+      let token = googleUser.getAuthResponse().id_token;
+
+      // console.log(profile);
+      // console.log(token);
+      this._usuarioService.loginGoogle(token).subscribe(resp => {
+        console.log(resp);
+        // this.router.navigate(['/dashboard']);
+        // Redireción manual
+        window.location.href = '#/dashboard';
+      });
+    });
+  }
+
+  ingresar(forma: NgForm) {
+
+    if (forma.invalid) {
+      return;
+    }
+
+    let usuario = new Usuario(null, forma.value.email, forma.value.password);
+
+    this._usuarioService.login(usuario, forma.value.recuerdame).subscribe( resp => {
+      this.router.navigate(['/dashboard']);
+    });
+
+    console.log(forma.valid);
+    console.log(forma.value);
+    // this.router.navigate(['/dashboard']);
   }
 
 }
